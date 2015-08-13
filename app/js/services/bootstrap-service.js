397828cc5724218576321cd4371aeec4dffc729e
Scott Burton <spurton@gmail.com>
2015-08-13 14:10:06 -0700

    more concise icon function

--- app/js/services/bootstrap-service.js
+++ app/js/services/bootstrap-service.js
@@ -8,18 +8,56 @@
         color = 'danger';
       } else if(status == 'paused') {
         color = 'warning';
+      } else if(status == 'active') {
+        color = 'success'; 
       };
       
       return color;
     },
     
-    this.icon = function(status) {      
-      return (status == 'active') ? 'glyphicon glyphicon-remove' : 'glyphicon glyphicon-play';
+    this.icon = function(status) {
+      var css = 'glyphicon '
+      
+      switch(status) {
+        case undefined:
+          break;
+        case 'active':
+          css += 'glyphicon-remove';
+          break;
+        case 'paused':
+          css += 'glyphicon-remove';
+          break;
+        case 'deactivated':
+          css += 'glyphicon-play';
+          break;
+      };
+
+      return css;
     },
     
     this.changeIcon = function(kind, element) {
       var classAttributes = BootStrapService.icon(attrs.action, $scope.feed.status);
       element.addClass(classAttributes);
+    },
+    
+    this.actionButtonClass = function(status) {
+      var css = 'btn btn-sm '
+      
+      switch(status) {
+        case undefined:
+          break;
+        case 'active':
+          css += 'btn-danger';
+          break;
+        case 'paused':
+          css += 'btn-danger';
+          break;
+        case 'deactivated':
+          css += 'btn-success';
+          break;
+      };
+
+      return css;
     }
   }
 
