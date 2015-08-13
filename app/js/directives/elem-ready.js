99accd76658ee41bb356615a97665d69f09915a8
Scott Burton <spurton@gmail.com>
2015-08-13 14:08:36 -0700

    element ready directive used to fire of things when an element loads

---
+++ app/js/directives/elem-ready.js
@@ -0,0 +1,15 @@
+angular
+  .module('socialExplosionAdmin')
+  .directive('elemReady', function( $parse ) {
+   return {
+     restrict: 'A',
+     link: function( $scope, elem, attrs ) {    
+        elem.ready(function(){
+          $scope.$apply(function(){
+            var func = $parse(attrs.elemReady);
+            func($scope);
+          })
+        })
+      }
+    }
+  });
