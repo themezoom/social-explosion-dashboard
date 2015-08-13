b90d156e254173c05a3ec4e7a66070e8c502242a
Scott Burton <spurton@gmail.com>
2015-08-13 14:08:15 -0700

    Add tooltip to index controller, Add show and edit controller plus functions and new modifications

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,17 +15,20 @@
         });
       };
     })
-    
-  .controller('RssFeedController', function (user, $state, $scope, RssFeedService, localStorageService) {
+  
+  
+  //INDEX
+  .controller('RssFeedsController', function (user, $state, $scope, RssFeedService, localStorageService) {
     if (user == null) {
       $state.go('login'); // There has to be a better way to do this
     };
     
     var feeds = RssFeedService.query({ token: user.token }, function() {
-      console.log(feeds);
+      $('[data-toggle="tooltip"]').tooltip();
     });
     
     $scope.rssFeeds = feeds;
+    $scope.user = user;
     
     $scope.toggleFeed = function(feed) {
       
@@ -46,14 +49,141 @@
     };
   })
   
-  .controller('NewRssFeedController', function (user, outletsService, $scope, RssFeedService, $stateParams, $state, FeedParserService) {
+  
+  //SHOW
+  .controller('ShowRssFeedController', function (user, $scope, $stateParams, RssFeedService, RssPromotionService, BootStrapService){
+    if (user == null) {
+      $state.go('login'); // There has to be a better way to do this
+    };
+    
+    var data = RssPromotionService.query({ token: user.token, id: $stateParams.id }, function(feed) {
+      console.log(feed);
+    });
+    
+    var feed = RssFeedService.get({ token: user.token, id: $stateParams.id }, function(feed) {      
+      if(feed.status == 'deactivated') {
+        $scope.title = 'Click to re-activate the feed';
+      } else if(feed.status == 'active') {
+        $scope.title = 'Click to deactivate the feed'; 
+      };
+      
+      var svg = dimple.newSvg("#chartContainer", '100%', 400);
+      var chart = new dimple.chart(svg, data);
+      chart.setBounds(0, 30, 605, 305);
+      chart.setMargins(50, 20, 50, 100);
+      var x = chart.addCategoryAxis("x", "date");
+      x.addOrderRule("date");
+      chart.addMeasureAxis("y", "promotions");
+      var s = chart.addSeries(feed.name, dimple.plot.line);
+      s.interpolation = "cardinal";
+      chart.draw();
+      
+      $scope.actionButtonClass = BootStrapService.actionButtonClass(feed.status);
+    });
+    
+    $scope.feed = feed;
+    
+    $scope.addToolTip = function() {
+      $('[data-toggle="tooltip"]').tooltip();
+    };
+    
+    $scope.toggleFeed = function(feed) {
+      
+      if (feed.status == 'deactivated') {
+        var update = feed.$update({ token: user.token, id: feed.id, rss_feed: { active: true } });
+      } else if(feed.status == 'active') {
+        var deactivated_on = new Date();
+        feed.$update({ token: user.token, id: feed.id, rss_feed: { active: false, deactivated_on:  deactivated_on } });
+      };
+    };
+    
+    $scope.toggleActiveSetting = function(id) {
+      //This is not ideal, Jquery stuff in here, this could be a directive
+      $('.tab-pane.active').removeClass('active');
+      $('#' + id).addClass('active');
+    };
+    
+  })
+
+  // EDIT
+  .controller('EditRssFeedController', function (user, $state, $scope, $filter, $stateParams, RssFeedService, outletsService, lodash) {
+    if (user == null) {
+      $state.go('login'); // There has to be a better way to do this
+    };    
+    
+    $scope.outlets = outletsService.outlets;
+    $scope.lodash = lodash;
+    $scope.feedCount = null;
+    $scope.notSaved = true;
+    $scope.dropDownOptions = [
+      'Low',
+      'Medium',
+      'High'
+    ];
+    
+    var feed = RssFeedService.get({ token: user.token, id: $stateParams.id }, function(feed) {
+      $scope.lodash.forEach($scope.outlets, function(outlet) {
+        $scope.lodash.forEach(feed.settings, function(item) {
+          if(item.name == outlet.name) {
+            outlet.freqSelected = item.frequency.charAt(0).toUpperCase() + item.frequency.slice(1);
+            outlet.durationSelected = item.duration.charAt(0).toUpperCase() + item.duration.slice(1);
+            outletsService.setDuration(outlet);
+            outletsService.setFrequency(outlet);
+          }
+        });
+      });
+      
+      $scope.totals = outletsService.reCalculate($scope.outlets, $scope.feedCount);
+    });
+    
+    $scope.feed = feed;
+    
+    $scope.updateRssFeed = function() {
+      var settings = $scope.outlets.map(function (outlet) {
+        var rObj = {};
+        if(outlet.enabled && (outlet.durationSelected != null) && (outlet.freqSelected != null)) {
+          rObj[outlet.name.toLowerCase()] = { reach: outlet.durationSelected.toLowerCase(), freq: outlet.freqSelected.toLowerCase() };
+        } else {
+          rObj[outlet.name.toLowerCase()] = { reach: '', freq: '' };
+        }
+        return rObj;
+      });
+
+      $scope.feed.$save({ token: user.token, rss_feed: { id: $scope.feed.id, url: $scope.feed.url, name: $scope.feed.name }, settings: JSON.stringify(settings) }, function() {
+          $scope.notSaved = false;
+          $scope.saved = true;
+      });
+    };
+    
+    $scope.selectFrequency = function(outlet) {
+      outletsService.setFrequency(outlet);
+    };
+    
+    $scope.selectDuration = function(outlet) {
+      outletsService.setDuration(outlet);
+    };
+    
+    $scope.toggleSelection = function(outlet) {
+      outletsService.reCalculate($scope.outlets, $scope.feedCount);
+    };
+  })
+  
+  
+  //NEW
+  .controller('NewRssFeedController', function (user, outletsService, $scope, RssFeedService, $stateParams, $state, FeedParserService, localStorageService, lodash) {
     if (user == null) {
       $state.go('login'); // There has to be a better way to do this
     };
     
     $scope.outlets = outletsService.outlets;
     $scope.feed = new RssFeedService();
+    $scope.localStorageService = localStorageService;
+    $scope.lodash = lodash;
     $scope.totals = 0;
+    $scope.defaults = $scope.localStorageService.get('defaults');
+    $scope.displayDefaultCheckbox = true;
+    
+    $scope.date = '20140313T00:00:00';
     
     $scope.addRssFeed = function() {
       var settings = $scope.outlets.map(function (outlet) {
@@ -66,7 +196,7 @@
         return rObj;
       });
 
-      $scope.feed.$create({ token: user.token, rss_feed: { url: $scope.feed.url }, settings: JSON.stringify(settings) }, function() {
+      $scope.feed.$create({ token: user.token, rss_feed: { url: $scope.feed.url, name: $scope.feed.name }, settings: JSON.stringify(settings) }, function() {
           $state.go('rssFeeds');
       });
     };
@@ -112,16 +242,11 @@
       $scope.totals = totals;
     };
     
-    $scope.selectFrequency = function(outlet, option) {
-      $scope.setFrequency(outlet, option);
-      $scope.calculate();
-    };
-    
-    $scope.setFrequency = function(outlet, value) {
+    $scope.selectFrequency = function(outlet) {
       var text = null;
       var val = null;
       
-      switch(value) {
+      switch(outlet.freqSelected) {
         case 'Low':
           text = '23-25'
           val = 24;
@@ -138,23 +263,19 @@
       
       outlet.freq = text;
       outlet.freqValue = val;
-      outlet.freqSelected = value;
-    };
-    
-    $scope.toggleSelection = function toggleSelection(outlet) {
-      outlet.status === 'enabled' ? outlet.status = 'disabled' : outlet.status = 'enabled'
-    };
-    
-    $scope.selectDuration = function(outlet, option) {
-      $scope.setDuration(outlet, option);
+      outlet.display = $scope.displayFooter(outlet);
       $scope.calculate();
     };
     
-    $scope.setDuration = function(outlet, value){
+    $scope.toggleSelection = function(outlet) {
+      $scope.calculate();
+    };
+    
+    $scope.selectDuration = function(outlet){
       var text = null;
       var val = null;
       
-      switch(value) {
+      switch(outlet.durationSelected) {
         case 'Low':
           text = '2-4'
           val = 3;
@@ -171,20 +292,89 @@
       
       outlet.duration = text;
       outlet.durationValue = val;
-      outlet.durationSelected = value;
+      outlet.display = $scope.displayFooter(outlet);
+      $scope.calculate();
     };
     
     $scope.displayFooter = function(outlet){
-      return outlet.status == 'enabled' && outlet.freq != null && outlet.duration != null;
+      return outletsService.shouldDisplayFooter(outlet);
     };
     
     
     // RSS Feed Parsing
     $scope.inputChanged = function() {
       FeedParserService.parse($scope.feed.url).then(function(res){
-        if(res.data.responseStatus == 200) $scope.feedCount = res.data.responseData.feed.entries.length;
+        if(res.data.responseStatus == 200) {
+          $scope.feedBroken = false;
+          $scope.feedOk = true;
+          $scope.feed.name = $scope.decodeHtml(res.data.responseData.feed.title); //TODO: Parse
+          $scope.feedCount = res.data.responseData.feed.entries.length;
+          $scope.calculate();
+          $scope.displayCalculations = true;
+        } else {
+          $scope.feedBroken = true;
+          $scope.feedOk = false;
+        }          
       });
     };
+    
+    $scope.decodeHtml = function(html){
+      var txt = document.createElement("textarea");
+      txt.innerHTML = html;
+      return txt.value;
+    };
+    
+    $scope.setDefault = function(outlet) {
+      //save the defaults somewhere and set them
+      if($scope.localStorageService.get('defaults') == null) {
+        var defaults = [];
+        defaults.push({
+          "name": outlet.name,
+          "durationSelected" : outlet.durationSelected,
+          "freqSelected" : outlet.freqSelected
+        });
+        
+        $scope.localStorageService.set('defaults', defaults);
+        console.log(defaults);
+      } else {
+        var defaults = $scope.localStorageService.get('defaults');
+        
+        $scope.lodash.forEach(defaults, function(item, index) {
+          if(item.name == outlet.name) {
+            defaults[index] = {
+              "name": outlet.name,
+              "durationSelected" : outlet.durationSelected,
+              "freqSelected" : outlet.freqSelected
+            }
+          } else {
+            defaults.push({
+              "name": outlet.name,
+              "durationSelected" : outlet.durationSelected,
+              "freqSelected" : outlet.freqSelected
+            })
+          }
+          
+          $scope.localStorageService.set('defaults', defaults);
+          console.log($scope.localStorageService.get('defaults'));
+        });
+      }
+    };
+    
+    // This loads the defaults for each outlet
+    $scope.mergeDefaults = function() {
+      $scope.lodash.forEach($scope.outlets, function(outlet) {
+        $scope.lodash.forEach($scope.defaults, function(item) {
+          if(item.name == outlet.name) {
+            outlet.freqSelected = item.freqSelected;
+            outlet.durationSelected = item.durationSelected;
+            $scope.selectDuration(outlet);
+            $scope.selectFrequency(outlet);
+          }
+        });
+      });
+      
+      $scope.calculate();
+    };    
     
   });
 })();
